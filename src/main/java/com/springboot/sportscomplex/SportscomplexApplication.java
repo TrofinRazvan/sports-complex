79688a249ecb1ff4e5317eedef7de399df384e57
Razvan <razvantrofin743@yahoo.com>
2023-05-23 14:45:55 +0300

    first commit

---
+++ src/main/java/com/springboot/sportscomplex/SportscomplexApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.sportscomplex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportscomplexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportscomplexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/sportscomplex/SportscomplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.sportscomplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportscomplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
